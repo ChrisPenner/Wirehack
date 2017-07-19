ee9912ac3d4ba207b93b40d172b26bda02aa11ba
Chris Penner <christopher.penner@gmail.com>
2017-07-18 19:51:07 -0600

    Add component monoid instance

--- src/Wirehack/Components.hs
+++ src/Wirehack/Components.hs
@@ -7,6 +7,11 @@ instance Show Component where
   show Empty = "."
   show All = "+"
 
+instance Monoid Component where
+  mempty = Empty
+  x `mappend` Empty = x
+  _ `mappend` x = x
+
 toggleC :: Component -> Component
 toggleC Empty = All
 toggleC All = Empty
